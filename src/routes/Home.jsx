6c046d51cd0e771f32d97e74f000088729dae08a
Matheus <limarios18@outlook.com>
2024-03-06 23:16:13 -0300

    Refatoriando o contexto em hook

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,15 +1,20 @@
 // 3 - alterando valor do contexto
-import { useContext } from "react";
-import { CounterContext } from "../context/CounterContext";
+// import { useContext } from "react";
+// import { CounterContext } from "../context/CounterContext";
+
 
 import ChangeCounter from "../components/ChangeCounter";
 
+// 4 refatorando com hook
+import { useCounterContext } from "../hooks/useCounterContext";
+
 const Home = () => {
-  const {counter} = useContext(CounterContext);
+  // const {counter} = useContext(CounterContext);
+
+  const { counter } = useCounterContext();
 
   return (
     <div>
-    
       <h1>Home</h1>
       <p>Valor do contador: {counter}</p>
       <ChangeCounter />
